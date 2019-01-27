bf88baa3b1268b54a6c97cf9e91ec0bd438ccb22
tutscp <tutscp@gmail.com>
2019-01-27 18:37:51 -0500

    m

--- out/_next/static/19oHw8fP_4fgUWSGaLshM/pages/index.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{204:function(n,e,t){__NEXT_REGISTER_PAGE("/",function(){return n.exports=t(205),{page:n.exports.default}})},205:function(n,e,t){"use strict";t.r(e);var o=t(1),u=t.n(o),r=t(50);e.default=function(){return u.a.createElement(r.a,null,"My home")}}},[[204,1,0]]]);
--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0499187983d8c9661ca1";
+/******/ 	var hotCurrentHash = "c4f7cbcca5f21ebbdfa2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
